604630b58b9f40ea307122cf6c91337724923031
Seth Curry <seth@scurry.io>
2025-05-20 23:05:59 -0400

    Declaring functions in user namespace

--- src/clash/core.clj
+++ src/clash/core.clj
@@ -1,7 +1,14 @@
 (ns clash.core
+  (:require [clojure.main :as main])
   (:gen-class))
 
+(in-ns 'user)
+(defn test-func []
+  (println "Hello, World!"))
+
+(in-ns 'clash.core)
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (do 
+  (main/main)))
